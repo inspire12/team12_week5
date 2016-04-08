144466b8bcb43fe71412ff76a0613a3988f0e7ad
seoyeonghak <ox4443@naver.com>
2016-04-08 12:03:24 +0900

    make AppVIew class
    by syh

---
+++ src/main/java/com/team12_week5/BillProgram/AppView.java
@@ -0,0 +1,12 @@
+package com.team12_week5.BillProgram;
+
+public class AppView {
+
+	public AppView() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void startMenu(){
+		System.out.println("안녕하세요. AppView 만들었습니다.");
+	}
+}
